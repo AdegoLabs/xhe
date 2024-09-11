51fe3f60a5264b8cf0febcf0cfd6520d28bda467
AdegoLabs <alphagoblin4@gmail.com>
2024-09-11 14:11:08 +0300

    Update onlinesimCloseOperationTest.php

--- test/onlinesimCloseOperationTest.php
+++ test/onlinesimCloseOperationTest.php
@@ -7,7 +7,7 @@ $container = require(__DIR__ . '/../app/bootstrap.php');
 $app = new Silly\Application();
 $app->useContainer($container, $injectWithTypeHint = true);
 
-$api = new \App\Classes\OnlineSim(new OnlineSimApi($key = 'ec2025fdadfe6b27b8f5e1cbbe7101da', 'RU'));
+$api = new \App\Classes\OnlineSim(new OnlineSimApi($key = '', 'RU'));
 
 $app->command('test:test', function() use(&$api) {
 	$tzid = ($api->getTzid('google')['tzid']);
